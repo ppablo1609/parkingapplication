ff0a6fd4c181d4c45f1b859cd97d79df58cb63aa
Pawel Karczewski <karczewski.pawel@hotmail.com>
2021-10-15 21:50:26 +0200

    Added Devise for User management

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,11 @@ ActiveRecord::Schema.define(version: 2021_10_14_194449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "spots", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+  # create_table "spots", force: :cascade do |t|
+  #   t.string "name"
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  # end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
